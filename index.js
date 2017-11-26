7432ef6ab8314dc24c0535ecc09bf12bab39bdb8
SsagorR <igogo800@gmail.com>
2017-11-26 23:44:50 +0200

    successful taken info from Yahoo Weather API

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-fetch('https://weather.com/weather/tenday/l/UPXX0017:1:UP')
-.then(function (response) {
-        return response.text();
-    })
-    .then(function (html) {
-    	console.log(html);
-    })
-    .catch(console.error);
+// fetch('https://weather.com/weather/tenday/l/UPXX0017:1:UP')
+// .then(function (response) {
+//         return response.text();
+//     })
+//     .then(function (html) {
+//     	var siteCode = document.createElement('html');
+//     	siteCode.innerHTML = html;
+//     	console.log(siteCode.getElementsByTagName('tr'));
+//     })
+//     .catch(console.error);
+
+    
+// 	'https://weather-ydn-yql.media.yahoo.com/forecastrss?w=924943&u=c'
+
+
+
+  var callbackFunction = function(data) {
+  	for (let i = 0; i <= 4; i++) {
+  		let dayInf = data.query.results.channel.item.forecast[i];
+  		let temp = "від " + dayInf.low + " до " + dayInf.high;
+  		let text = dayInf.text;
+  		let tempIns = document.getElementById('temp' + i);
+  		let textIns = document.getElementById('text' + i);
+  		textIns.innerHTML = text;
+  		tempIns.innerHTML = temp;
+  	}
+
+  };
+
+
+
+
+
 
 // function httpGet(theUrl) {
 //     let xmlHttp = null;
