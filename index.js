c27f24b734b0d5abbc6909c83a1395da59f7066e
SsagorR <igogo800@gmail.com>
2017-11-27 23:24:15 +0200

    updated loading weather with fetch

--- index.js
+++ index.js
@@ -12,20 +12,25 @@
     
 // 	'https://weather-ydn-yql.media.yahoo.com/forecastrss?w=924943&u=c'
 
-
-
-  var callbackFunction = function(data) {
+let a = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(924943)%20and%20u=%27c%27&format=json` 
+fetch(a).then(res=>res.json()).then(function(data) {
   	for (let i = 0; i <= 4; i++) {
   		let dayInf = data.query.results.channel.item.forecast[i];
   		let temp = "від " + dayInf.low + " до " + dayInf.high;
   		let text = dayInf.text;
+  		let date = dayInf.date;
   		let tempIns = document.getElementById('temp' + i);
   		let textIns = document.getElementById('text' + i);
+  		let dateIns = document.getElementById('date' + i);
+
   		textIns.innerHTML = text;
   		tempIns.innerHTML = temp;
+  		dateIns.innerHTML = date;
   	}
 
-  };
+  });
+
+  // var callbackFunction = 
 
 
 
